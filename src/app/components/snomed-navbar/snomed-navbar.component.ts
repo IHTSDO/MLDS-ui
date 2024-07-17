e8570c8fb084668867b7f212dffb35b4c8dd54d2
Peter Duff <peterduff@gmail.com>
2024-07-17 15:58:09 +0100

    MLDS-1057 Cleaning

--- src/app/components/snomed-navbar/snomed-navbar.component.ts
+++ src/app/components/snomed-navbar/snomed-navbar.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
 import {Location} from '@angular/common';
 
 @Component({
@@ -11,21 +10,6 @@ export class SnomedNavbarComponent implements OnInit {
 
     environment: string;
     path: string;
-
-    branches: any;
-    branchesSubscription: Subscription;
-    activeBranch: any;
-    activeBranchSubscription: Subscription;
-
-    projects: any;
-    projectsSubscription: Subscription;
-    activeProject: any;
-    activeProjectSubscription: Subscription;
-
-    tasks: any;
-    tasksSubscription: Subscription;
-    activeTask: any;
-    activeTaskSubscription: Subscription;
 
     constructor(private location: Location) {
         this.environment = window.location.host.split(/[.]/)[0].split(/[-]/)[0];
