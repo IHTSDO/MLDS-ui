72e21e2a324c3451aeb32dc57d60c6e4c00ce0cb
jaykamal <130353187+jaykamal@users.noreply.github.com>
2024-08-16 14:44:40 +0530

    MLDS-1033 Updated Environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api',
-  apiBaseUrl: 'http://localhost:8080'
+  apiUrl: 'https://dev-mlds.ihtsdotools.org/api',
+  apiBaseUrl: 'https://dev-mlds.ihtsdotools.org'
 };
 
 /*
