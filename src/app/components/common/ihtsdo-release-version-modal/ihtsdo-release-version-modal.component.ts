17461d14010d6d359e2e0fa39432b3e9a0577a2c
jaykamal <jayakamala.n@telliant.net>
2025-01-30 17:32:40 +0530

    MLDS-1198
    UI/UX Style improvements - user side and admin releases links

--- src/app/components/common/ihtsdo-release-version-modal/ihtsdo-release-version-modal.component.ts
+++ src/app/components/common/ihtsdo-release-version-modal/ihtsdo-release-version-modal.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { CompareTextPipe } from 'src/app/pipes/compare-text/compare-text.pipe';
+import { RemoveHtmlPipe } from 'src/app/pipes/remove-html/remove-html.pipe';
 
 @Component({
   selector: 'app-ihtsdo-release-version-modal',
   standalone: true,
-  imports: [CommonModule,TranslateModule,CompareTextPipe],
+  imports: [CommonModule,TranslateModule,CompareTextPipe,RemoveHtmlPipe],
   templateUrl: './ihtsdo-release-version-modal.component.html',
   styleUrl: './ihtsdo-release-version-modal.component.scss'
 })
