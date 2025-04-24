1c6b9cc4962d88171129f24385ef9a33a3093d30
AravinthAasai <109154990+AravinthAasaithambi@users.noreply.github.com>
2025-04-24 18:27:07 +0530

    MLDS-1222 MLDS-UI: Automate maintenance of site footer flags from Members data

--- src/app/components/common/landing-footer/landing-footer.component.ts
+++ src/app/components/common/landing-footer/landing-footer.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 import { MemberService } from 'src/app/services/member/member.service';
 import { CompareTextPipe } from "../../../pipes/compare-text/compare-text.pipe";
+import { CommonModule } from '@angular/common';
 
 /**
  * Landing Footer Component
@@ -16,7 +17,7 @@ import { CompareTextPipe } from "../../../pipes/compare-text/compare-text.pipe";
 @Component({
   selector: 'app-landing-footer',
   standalone: true,
-  imports: [TranslateModule, CompareTextPipe],
+  imports: [TranslateModule, CompareTextPipe,CommonModule],
   templateUrl: './landing-footer.component.html',
   styleUrl: './landing-footer.component.scss'
 })
@@ -28,7 +29,8 @@ export class LandingFooterComponent {
    * The current year is used to display the copyright information in the footer.
    */
   currentYear: number;
-
+  isLoading: boolean = true;
+  members: any[] = [];
   /**
    * Constructor
    * 
@@ -40,5 +42,39 @@ export class LandingFooterComponent {
   constructor(private memberService: MemberService, private route: ActivatedRoute) {
     this.currentYear = new Date().getFullYear();
   }
+  ngOnInit(): void {
+    this.fetchMembers(); // Fetch members when component loads
+  }
 
+  fetchMembers(): void {
+    this.isLoading = true;
+    this.memberService.getMembers().subscribe({
+      next: (data) => {
+        // Filter out 'ihtsdo'
+        const filteredMembers = data.filter((member: any) => member.key.toLowerCase() !== 'ihtsdo');
+        this.members = this.sortMembers(filteredMembers);
+        this.isLoading = false;
+      },
+      error: (error) => {
+        this.isLoading = false;
+        console.error('Error fetching members:', error);
+      }
+    });
+  }
+  
+// Function to sort members by key (alpha-2 code)
+private sortMembers(members: any[]): any[] {
+  return members.sort((a, b) => {
+    return a.key.localeCompare(b.key);
+  });
+}
+
+  getMemberLandingPage(member: any): string {
+    let fullBaseUrl = `${window.location.protocol}//${window.location.host}/#/landing/${member.key}`;
+    if (member.language) {
+      fullBaseUrl += `?lang=${member.language}`;
+    }
+    return fullBaseUrl;
+  }
+    
 }
